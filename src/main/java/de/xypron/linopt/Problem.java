e24d7ae44c83d4c500e83b9823fe419a507685b8
Heinrich Schuchardt <xypron.glpk@gmx.de>
2010-10-07 19:01:12 +0000

    ''
    git-svn-id: https://www.xypron.de/svn/linopt/trunk@218 84c2c103-35b1-4711-a580-c5e297110a67

---
+++ src/main/java/de/xypron/linopt/Problem.java
@@ -0,0 +1,527 @@
+package de.xypron.linopt;
+
+
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Heinrich Schuchardt
+ */
+public class Problem {
+
+    private String name;
+    private TreeMap<String, Column> columns =
+            new TreeMap<String, Column>();
+    private Objective objectiveFunction = null;
+    private TreeMap<Row, TreeMap<Column, Double>> matrix =
+            new TreeMap<Row, TreeMap<Column, Double>>();
+    private TreeMap<String, Row> rows =
+            new TreeMap<String, Row>();
+
+    /**
+     * Optimization direction
+     */
+    public enum Direction {
+
+        MINIMIZE,
+        MAXIMIZE
+    }
+
+    /**
+     * Column type
+     */
+    public enum ColumnType {
+
+        FLOAT,
+        INTEGER,
+        BINARY
+    }
+
+    /**
+     * Get problem name
+     * @return problem name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set problem name
+     * @param name problem name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get columns
+     * @return columns
+     */
+    public TreeSet<Column> getColumns() {
+        TreeSet<Column> ret = new TreeSet<Column>();
+        for (Entry<String, Column> e : columns.entrySet()) {
+            ret.add(e.getValue());
+        }
+        return ret;
+    }
+
+    /**
+     * Get matrix
+     * @return matrix
+     */
+    public TreeMap<Row, TreeMap<Column, Double>> getMatrix() {
+        return matrix;
+    }
+
+    /**
+     * Get objective function
+     * @return objective function
+     */
+    public Objective getObjective() {
+        return objectiveFunction;
+    }
+
+    /**
+     * Get rows (including objective)
+     * @return rows
+     */
+    public TreeSet<Row> getRows() {
+        TreeSet<Row> ret = new TreeSet<Row>();
+        for (Entry<String, Row> e : rows.entrySet()) {
+            ret.add(e.getValue());
+        }
+        return ret;
+    }
+
+    /**
+     * Get column identified by name and indices
+     * @param name column name
+     * @param index indices
+     * @return
+     */
+    public Column column(String name, Object... index) {
+        Column ret;
+        String key = key(name, index);
+        if (columns.containsKey(key)) {
+            ret = columns.get(key);
+        } else {
+            ret = new Column(key);
+        }
+        return ret;
+    }
+
+    /**
+     * Create objective function
+     * @param name name
+     * @param direction optimization direction
+     * @return
+     */
+    public Objective objective(String name, Direction direction) {
+        if (objectiveFunction == null) {
+            objectiveFunction = new Objective(name, direction);
+        } else {
+            throw new RuntimeException("Objective already defined.");
+        }
+        return objectiveFunction;
+    }
+
+    /**
+     * Get row identified by name and indices
+     * @param name
+     * @param index
+     * @return
+     */
+    public Row row(String name, Object... index) {
+        Row ret;
+        String key = key(name, index);
+        if (rows.containsKey(key)) {
+            ret = rows.get(key);
+        } else {
+            ret = new Row(key);
+        }
+        return ret;
+    }
+
+    /**
+     * Create key for column or row
+     * @param name
+     * @param index
+     * @return
+     */
+    private String key(String name, Object... index) {
+        String sep = "(";
+        String key = name;
+        if (index.length > 0) {
+            for (Object i : index) {
+                key += sep;
+                key += i;
+                sep = ",";
+            }
+            key += ")";
+        }
+        return key;
+    }
+
+    /**
+     * Column
+     */
+    public class Column implements Comparable<Column> {
+
+        private String key;
+        private ColumnType type = null;
+        private Double lowerBound = null;
+        private Double upperBound = null;
+        private int columnNumber;
+        private double value;
+        private double dual;
+
+        private Column(String key) {
+            this.key = key;
+            initialize();
+        }
+
+        private void initialize() {
+            if (columns.containsKey(key)) {
+                throw new RuntimeException("Column " + key + " already exists.");
+            }
+            columns.put(key, this);
+        }
+
+        /**
+         * Get column name
+         * @return name
+         */
+        public String getKey() {
+            return key;
+        }
+
+        /**
+         * Get column type
+         * @return type
+         */
+        public ColumnType getType() {
+            return type;
+        }
+
+        /**
+         * Get dual value
+         * @return dual
+         */
+        public double getDual() {
+            return dual;
+        }
+
+        /**
+         * Set dual value
+         * @param dual
+         */
+        public void setDual(double dual) {
+            this.dual = dual;
+        }
+
+        /**
+         * Get lower bound
+         * @return lower bound
+         */
+        public Double getLowerBound() {
+            return lowerBound;
+        }
+
+        /**
+         * Set lower bound
+         * @param lowerBound lower bound
+         */
+        public void setLowerBound(Double lowerBound) {
+            this.lowerBound = lowerBound;
+        }
+
+        /**
+         * Get column number
+         * @return column number
+         */
+        public int getColumnNumber() {
+            return columnNumber;
+        }
+
+        /**
+         * Set column number
+         * @param columnNumber column number
+         */
+        public void setColumnNumber(int columnNumber) {
+            this.columnNumber = columnNumber;
+        }
+
+        /**
+         * Get upper bound
+         * @return upper bound
+         */
+        public Double getUpperBound() {
+            return upperBound;
+        }
+
+        /**
+         * Set upper bound
+         * @param upperBound upper bound
+         */
+        public void setUpperBound(Double upperBound) {
+            this.upperBound = upperBound;
+        }
+
+        /**
+         * Get value
+         * @return value
+         */
+        public double getValue() {
+            return value;
+        }
+
+        /**
+         * Set value
+         * @param value value
+         */
+        public void setValue(double value) {
+            this.value = value;
+        }
+
+        public Column bounds(Double lowerBound, Double upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+            return this;
+        }
+
+        /**
+         * Set column type
+         * @param type column type
+         * @return column
+         */
+        public Column type(ColumnType type) {
+            this.type = type;
+            return this;
+        }
+
+        /**
+         * Set coefficient
+         * @param value coefficient
+         * @param rowName row name
+         * @param index row indices
+         * @return
+         */
+        public Column add(double value, String rowName, Object... index) {
+            return add(value, row(rowName, index));
+        }
+
+        /**
+         * Set coefficient
+         * @param value value
+         * @param row row
+         * @return
+         */
+        public Column add(double value, Row row) {
+            matrix.get(row).put(this, value);
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return key;
+        }
+
+        @Override
+        public int compareTo(Column o) {
+            return key.compareTo(o.key);
+        }
+    }
+
+    public class Objective
+            extends Row {
+
+        private Direction direction;
+
+        /**
+         * Create objective
+         * @param key name
+         * @param direction optimization direction
+         */
+        Objective(String key, Direction direction) {
+            super(key);
+            this.direction = direction;
+        }
+
+        /**
+         * Get optimization direction
+         * @return optimization direction
+         */
+        public Direction getDirection() {
+            return direction;
+        }
+
+        /**
+         * Set optimization direction
+         * @param direction optimization direction
+         */
+        public void setDirection(Direction direction) {
+            this.direction = direction;
+        }
+
+    }
+
+    /**
+     * Row
+     */
+    public class Row implements Comparable<Row> {
+
+        private String key;
+        private Double lowerBound = null;
+        private Double upperBound = null;
+        private int rowNumber;
+        private double value;
+        private double dual;
+
+        private Row(String key) {
+            this.key = key;
+            initialize();
+        }
+
+        private void initialize() {
+            if (rows.containsKey(key)) {
+                throw new RuntimeException("Row " + key + " already exists.");
+            }
+            rows.put(key, this);
+            matrix.put(this, new TreeMap<Column, Double>());
+        }
+
+        /**
+         * Get row name
+         * @return name
+         */
+        public String getKey() {
+            return key;
+        }
+
+        /**
+         * Get row dual value
+         * @return dual
+         */
+        public double getDual() {
+            return dual;
+        }
+
+        /**
+         * Set row dual value
+         * @param dual dual
+         */
+        public void setDual(double dual) {
+            this.dual = dual;
+        }
+
+        /**
+         * Get lower bound
+         * @return lower bound
+         */
+        public Double getLowerBound() {
+            return lowerBound;
+        }
+
+        /**
+         * Set lower bound
+         * @param lowerBound lower bound
+         */
+        public void setLowerBound(Double lowerBound) {
+            this.lowerBound = lowerBound;
+        }
+
+        /**
+         * Get row number
+         * @return row number
+         */
+        public int getRowNumber() {
+            return rowNumber;
+        }
+
+        /**
+         * Set row number
+         * @param rowNumber row number
+         */
+        public void setRowNumber(int rowNumber) {
+            this.rowNumber = rowNumber;
+        }
+
+        /**
+         * Get upper bound
+         * @return upper bound
+         */
+        public Double getUpperBound() {
+            return upperBound;
+        }
+
+        /**
+         * Set upper bound
+         * @param upperBound upper bound
+         */
+        public void setUpperBound(Double upperBound) {
+            this.upperBound = upperBound;
+        }
+
+        /**
+         * Get value
+         * @return value
+         */
+        public double getValue() {
+            return value;
+        }
+
+        /**
+         * Set value
+         * @param value value
+         */
+        public void setValue(double value) {
+            this.value = value;
+        }
+
+        /**
+         * Set coefficient
+         * @param value coefficient
+         * @param columnName column name
+         * @param index column indices
+         * @return this row
+         */
+        public Row add(double value, String columnName, Object... index) {
+            return add(value, column(columnName, index));
+        }
+
+        /**
+         * Set coefficient
+         * @param value value
+         * @param column column
+         * @return this row
+         */
+        public Row add(double value, Column column) {
+            matrix.get(this).put(column, value);
+            return this;
+        }
+
+        /**
+         * Set bounds
+         * @param lowerBound lower bound
+         * @param upperBound upper bound
+         * @return this row
+         */
+        public Row bounds(Double lowerBound, Double upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return key;
+        }
+
+        @Override
+        public int compareTo(Row o) {
+            return key.compareTo(o.key);
+        }
+    }
+}
---
+++ src/main/java/de/xypron/linopt/examples/CutSimple.java
@@ -0,0 +1,116 @@
+package de.xypron.linopt.examples;
+
+
+import de.xypron.linopt.Solver;
+import de.xypron.linopt.Problem;
+import de.xypron.linopt.SolverGlpk;
+
+/*
+ *  Copyright (C) 2010 Heinrich Schuchardt
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This program solves a small cutting stock problem.
+ * For solving usual size problems column generation should be used.
+ * @author Heinrich Schuchardt
+ */
+public class CutSimple {
+
+    public static void main(String[] args) {
+        final String COLUMN_USE = "u";
+        final String COLUMN_CUT = "x";
+        final String OBJECTIVE = "waste";
+        final String PROBLEM = "CuttingStock";
+        final String ROW_DEMAND = "demand";
+        final String ROW_STOCK = "stock";
+
+        Solver s = new SolverGlpk();
+        Problem p;
+        // lengths of stock items
+        long[] stock = {50, 50, 100, 100, 100, 200, 200};
+        // sales products
+        long[] product = {6, 10, 16, 25, 40, 63};
+        // demand per sales product
+        long[] demand = {20, 10, 3, 7, 2, 1};
+
+        // define problem
+        p = new Problem();
+        p.setName(PROBLEM);
+
+        // define columns
+        for (int i = 0; i < stock.length; i++) {
+            // u(i) : stock element i is used
+            p.column(COLUMN_USE, i).type(Problem.ColumnType.BINARY);
+            for (int j = 0; j < product.length; j++) {
+                // x(i,j) : x pieces of product j are cut from stock i
+                p.column(COLUMN_CUT, i, j).
+                        type(Problem.ColumnType.INTEGER).
+                        bounds(0., null);
+            }
+        }
+        // define objective
+        p.objective(OBJECTIVE, Problem.Direction.MINIMIZE);
+        for (int i = 0; i < stock.length; i++) {
+            // Minimize waste :
+            //     stockLength(i) * u(i) - sum( productLength(j) * x(i,j) )
+            p.getObjective().add(stock[i], COLUMN_USE, i);
+            for (int j = 0; j < product.length; j++) {
+                p.getObjective().add(-product[j], COLUMN_CUT, i, j);
+            }
+        }
+        // define rows
+        for (int i = 0; i < stock.length; i++) {
+            // stockLength(i) * u(i) >= sum( productLength[j] * x(i,j)
+            p.row(ROW_STOCK, i).bounds(0., null).
+                    add(stock[i], COLUMN_USE, i);
+            for (int j = 0; j < product.length; j++) {
+                p.row(ROW_STOCK, i).add(-product[j], COLUMN_CUT, i, j);
+            }
+        }
+        for (int j = 0; j < product.length; j++) {
+            // demand(j) = sum( x(i,j) )
+            p.row(ROW_DEMAND, j).bounds((double) demand[j], (double) demand[j]);
+            for (int i = 0; i < stock.length; i++) {
+                p.row(ROW_DEMAND, j).add(1., COLUMN_CUT, i, j);
+            }
+        }
+
+        // solve
+        s.solve(p);
+
+        // output solution
+        System.out.println("Production plan");
+        for (int i = 0; i < stock.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < product.length; j++) {
+                double v = p.column(COLUMN_CUT, i, j).getValue();
+                if (v > 0) {
+                    if (flag) {
+                        System.out.print(" + ");
+                    } else {
+                        System.out.print(stock[i] + " >= ");
+                    }
+                    System.out.print(v + " * " + product[j]);
+                    flag = true;
+                }
+            }
+            if (flag) {
+                System.out.println();
+            }
+        }
+        System.out.println("Waste = " + p.getObjective().getValue());
+    }
+}
